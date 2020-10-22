01e32e8737843f6e74849326b1f86e3dd6e22761
Zsolt Vizi <vizizsolt89@gmail.com>
2020-10-22 19:54:44 +0200

    Create basis repository

---
+++ model.py
@@ -0,0 +1,115 @@
+import numpy as np
+from scipy.integrate import odeint
+
+
+class RostModelHungary:
+    def __init__(self, model_data):
+        self.contact_mtx = model_data.contact_data
+        self.population = model_data.age_data.flatten()
+        self.age_group_data = model_data.age_group_data
+
+        self.compartments = ["s", "l1", "l2",
+                             "ip", "ia1", "ia2", "ia3",
+                             "is1", "is2", "is3",
+                             "ih", "ic", "icr",
+                             "r", "d", "c"]
+        self.c_idx = {comp: idx for idx, comp in enumerate(self.compartments)}
+        self.n_age = self.population.shape[0]
+
+    def get_initial_values(self):
+        iv = {
+            "l1": np.zeros(self.n_age),
+            "l2": np.zeros(self.n_age),
+
+            "ip": np.zeros(self.n_age),
+
+            "ia1": np.zeros(self.n_age),
+            "ia2": np.zeros(self.n_age),
+            "ia3": np.zeros(self.n_age),
+
+            "is1": np.zeros(self.n_age),
+            "is2": np.zeros(self.n_age),
+            "is3": np.zeros(self.n_age),
+
+            "ih": np.zeros(self.n_age),
+            "ic": np.zeros(self.n_age),
+            "icr": np.zeros(self.n_age),
+
+            "d": np.zeros(self.n_age),
+            "r": np.zeros(self.n_age)
+        }
+        iv["l1"][2] = 1
+        iv.update({
+            "c": iv["ip"] + iv["ia1"] + iv["ia2"] + iv["ia3"] + iv["is1"] + iv["is2"] + iv["is3"] + iv["r"] + iv["d"]
+        })
+        iv.update({
+            "s": self.population - (iv["c"] + iv["l1"] + iv["l2"])
+        })
+
+        return np.array([iv[comp] for comp in self.compartments]).flatten()
+
+    def get_solution(self, t, parameters):
+        initial_values = self.get_initial_values()
+        return np.array(odeint(self.get_model, initial_values, t, args=(parameters,)))
+
+    def get_model(self, xs, _, ps):
+        # the same order as in self.compartments!
+        s, l1, l2, ip, ia1, ia2, ia3, is1, is2, is3, ih, ic, icr, r, d, c = xs.reshape(-1, 10)
+
+        transmission = ps["beta"] * \
+            np.array((ip + ps["inf_a"] * (ia1 + ia2 + ia3) + (is1 + is2 + is3))).dot(self.contact_mtx)
+        actual_population = self.population
+
+        # TODO: implement old RostModelHungary
+        model_eq_dict = {
+            "s": -ps["susc"] * (s / actual_population) * transmission,  # S'(t)
+            "l1": ps["susc"] * (s / actual_population) * transmission - 2 * ps["alpha_l"] * l1,  # L1'(t)
+            "l2": 2 * ps["alpha_l"] * l1 - 2 * ps["alpha_l"] * l2,  # L2'(t)
+
+            "ip": (1 - ps["p"]) * 2 * ps["alpha_l"] * l2 - ps["alpha_p"] * ip,  # Ip'(t)
+
+            "ia1": ps["p"] * 2 * ps["alpha_l"] * l2 - 3 * ps["gamma_a"] * ia1,  # Ia1'(t)
+            "ia2": 3 * ps["gamma_a"] * ia1 - 3 * ps["gamma_a"] * ia2,  # Ia2'(t)
+            "ia3": 3 * ps["gamma_a"] * ia2 - 3 * ps["gamma_a"] * ia3,  # Ia3'(t)
+
+            "is1": ps["alpha_p"] * ip - 3 * ps["gamma_s"] * is1,  # Is1'(t)
+            "is2": 3 * ps["gamma_s"] * is1 - 3 * ps["gamma_s"] * is2,  # Is2'(t)
+            "is3": 3 * ps["gamma_s"] * is2 - 3 * ps["gamma_s"] * is3,  # Is3'(t)
+
+            "ih": ps["factor_ih"] * ps["h"] * (1 - ps["factor_ic"] * ps["xi"]) * 3 * ps["gamma_s"] * is3
+            - ps["gamma_h"] * ih,  # Ih'(t)
+            "ic": ps["factor_ih"] * ps["h"] * ps["factor_ic"] * ps["xi"] * 3 * ps["gamma_s"] * is3
+            - ps["gamma_c"] * ic,  # Ic'(t)
+            "icr": (1 - ps["factor_d"] * ps["mu"]) * ps["gamma_c"] * ic - ps["gamma_cr"] * icr,  # Icr'(t)
+
+            "r": 3 * ps["gamma_a"] * ia3 + (1 - ps["factor_ih"] * ps["h"]) * 3 * ps["gamma_s"] * is3
+            + ps["gamma_h"] * ih + ps["gamma_cr"] * icr,  # R'(t)
+            "d": ps["factor_d"] * ps["mu"] * ps["gamma_c"] * ic,  # D'(t)
+
+            "c": 2 * ps["alpha_l"] * l2  # C'(t)
+        }
+
+        model_eq = [model_eq_dict[comp] for comp in self.compartments]
+        v = np.array(model_eq).flatten()
+        return v
+
+    @staticmethod
+    def aggregate_by_age(solution, idx):
+        return np.sum(solution[:, idx * 10:(idx + 1) * 10], axis=1)
+
+    def get_cumulative(self, solution):
+        idx = self.c_idx["c"]
+        return self.aggregate_by_age(solution, idx)
+
+    def get_deaths(self, solution):
+        idx = self.c_idx["d"]
+        return self.aggregate_by_age(solution, idx)
+
+    def get_hospitalized(self, solution):
+        idx = self.c_idx["ih"]
+        idx_2 = self.c_idx["icr"]
+        return self.aggregate_by_age(solution, idx) + self.aggregate_by_age(solution, idx_2)
+
+    def get_ventilated(self, solution):
+        idx = self.c_idx["ic"]
+        return self.aggregate_by_age(solution, idx)
---
+++ r0.py
@@ -0,0 +1,89 @@
+from typing import List
+
+import numpy as np
+
+
+class R0Generator:
+    states = ["l1", "l2", "ip", "a1", "a2", "a3", "i1", "i2", "i3"]
+
+    def __init__(self, param: dict, n_age: int = 10, n_l: int = 2, n_a: int = 3, n_i: int = 3):
+        self.parameters = param
+        self.n_age = n_age
+        self.n_l = n_l
+        self.n_a = n_a
+        self.n_i = n_i
+        self.n_states = len(self.states)
+        self.i = {self.states[index]: index for index in np.arange(0, self.n_states)}
+        self.s_mtx = self.n_age * self.n_states
+
+        self.v_inv = None
+        self.__get_v()
+
+    def get_eig_val(self, contact_mtx: np.array, susceptibles: np.ndarray, population: np.ndarray) -> List[np.float]:
+        # contact matrix needed for effective reproduction number: [c_{j,i} * S_i(t) / N_i(t)]
+        contact_matrix = contact_mtx / population.reshape((-1, 1))
+        cm_tensor = np.tile(contact_matrix, (susceptibles.shape[0], 1, 1))
+        susc_tensor = susceptibles.reshape((susceptibles.shape[0], 1, susceptibles.shape[1]))
+        contact_matrix_tensor = cm_tensor * susc_tensor
+        r_eff = []
+        for cm in contact_matrix_tensor:
+            f = self.__get_f(cm)
+            r_eff.append(np.linalg.eig(np.dot(f, self.v_inv))[0][0])
+        return r_eff
+
+    def __get_v(self) -> np.array:
+        # TODO: implement old model
+        idx = self.__idx
+        v = np.zeros((self.n_age * self.n_states, self.n_age * self.n_states))
+        # L1 -> L2
+        v[idx("l1"), idx("l1")] = self.n_l * self.parameters["alpha_l"]
+        v[idx("l2"), idx("l1")] = -self.n_l * self.parameters["alpha_l"]
+        # L2 -> Ip/A1
+        v[idx("l2"), idx("l2")] = self.n_l * self.parameters["alpha_l"]
+        v[idx("ip"), idx("l2")] = -self.n_l * self.parameters["alpha_l"] * (1 - self.parameters["p"])  # !
+        v[idx("a1"), idx("l2")] = -self.n_l * self.parameters["alpha_l"] * self.parameters["p"]  # !
+        # ip -> I1
+        v[idx("ip"), idx("ip")] = self.parameters["alpha_p"]
+        v[idx("i1"), idx("ip")] = -self.parameters["alpha_p"]  # !
+        # A1 -> A2
+        v[idx("a1"), idx("a1")] = self.n_a * self.parameters["gamma_a"]
+        v[idx("a2"), idx("a1")] = -self.n_a * self.parameters["gamma_a"]
+        # A2 -> A3
+        v[idx("a2"), idx("a2")] = self.n_a * self.parameters["gamma_a"]
+        v[idx("a3"), idx("a2")] = -self.n_a * self.parameters["gamma_a"]
+        # A3 -> R
+        v[idx("a3"), idx("a3")] = self.n_a * self.parameters["gamma_a"]
+        # I1 -> I2
+        v[idx("i1"), idx("i1")] = self.n_i * self.parameters["gamma_s"]
+        v[idx("i2"), idx("i1")] = -self.n_i * self.parameters["gamma_s"]
+        # I2 -> I3
+        v[idx("i2"), idx("i2")] = self.n_i * self.parameters["gamma_s"]
+        v[idx("i3"), idx("i2")] = -self.n_i * self.parameters["gamma_s"]
+        # I3 -> Ih/Ic (& R)
+        v[idx("i3"), idx("i3")] = self.n_i * self.parameters["gamma_s"]
+
+        self.v_inv = np.linalg.inv(v)
+
+    def __get_f(self, contact_mtx: np.array) -> np.array:
+        i = self.i
+        s_mtx = self.s_mtx
+        n_states = self.n_states
+
+        f = np.zeros((self.n_age * n_states, self.n_age * n_states))
+        inf_a = self.parameters["inf_a"] if "inf_a" in self.parameters.keys() else 1.0
+        inf_s = self.parameters["inf_s"] if "inf_s" in self.parameters.keys() else 1.0
+        inf_p = self.parameters["inf_p"] if "inf_p" in self.parameters.keys() else 1.0
+
+        susc_vec = self.parameters["susc"].reshape((-1, 1))
+        f[i["l1"]:s_mtx:n_states, i["ip"]:s_mtx:n_states] = inf_p * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["a1"]:s_mtx:n_states] = inf_a * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["a2"]:s_mtx:n_states] = inf_a * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["a3"]:s_mtx:n_states] = inf_a * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["i1"]:s_mtx:n_states] = inf_s * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["i2"]:s_mtx:n_states] = inf_s * contact_mtx.T * susc_vec
+        f[i["l1"]:s_mtx:n_states, i["i3"]:s_mtx:n_states] = inf_s * contact_mtx.T * susc_vec
+
+        return f
+
+    def __idx(self, state: str) -> int:
+        return np.arange(self.n_age * self.n_states) % self.n_states == self.i[state]
