db4cee6c89d7238034c71c10ecd0159ec2bd44c8
BNorbert88 <bnorbert88@gmail.com>
2020-11-01 16:28:43 +0100

    Add basic dynamic plotter function

--- main.py
+++ main.py
@@ -12,58 +12,53 @@ from r0 import R0Generator
 
 import plotter
 
+# Load data
+data = DataLoader()
+
+# Define initial configs
+susc_choices = [0.5]  # [0.1, 0.5, 1.0]
+r0_choices = [2.2]  # [1.65, 2.2, 3]
+lower_matrix_types = ["home"]  # ["home", "normed"]
+no_ag = data.contact_data["home"].shape[0]
+model = RostModelHungary(model_data=data)
+population = model.population
+age_vector = population.reshape((-1, 1))
+susceptibles = model.get_initial_values()[model.c_idx["s"] * no_ag:(model.c_idx["s"] + 1) * no_ag]
+
+contact_matrix = data.contact_data["home"] + data.contact_data["work"] + \
+                 data.contact_data["school"] + data.contact_data["other"]
+contact_home = data.contact_data["home"]
+upper_limit_matrix = contact_matrix * age_vector
+lower_limit_matrices = {"normed": upper_limit_matrix - np.min(upper_limit_matrix - contact_home * age_vector),
+                        "home": contact_home * age_vector}
+upper_tri_indexes = np.triu_indices(no_ag)
+
 
 def main():
-    # Load data
-    data = DataLoader()
-
-    # Define initial configs
-    susc_choices = [0.1, 0.5, 1.0]
-    r0_choices = [1.65, 2.2, 3]
-    lower_matrix_types = ["home", "normed"]
-    no_ag = data.contact_data["home"].shape[0]
-
+    is_lhs_generated = False
     # Instantiate model
-    model = RostModelHungary(model_data=data)
-    s_idx = model.c_idx["s"]
-    ic_idx = model.c_idx["ic"]
-    susceptibles = model.get_initial_values()[s_idx * no_ag:(s_idx + 1) * no_ag]
-    population = model.population
-    age_vector = population.reshape((-1, 1))
+    # ic_idx = model.c_idx["ic"]
 
     # Get contact matrices
-    contact_matrix = \
-        data.contact_data["home"] + data.contact_data["work"] + \
-        data.contact_data["school"] + data.contact_data["other"]
-    contact_home = data.contact_data["home"]
-    upper_limit_matrix = contact_matrix * age_vector
-    lower_limit_matrices = {"normed": upper_limit_matrix - np.min(upper_limit_matrix - contact_home * age_vector),
-                            "home": contact_home * age_vector}
-    upper_tri_indexes = np.triu_indices(no_ag)
+
 
     # --- Get complete parameters dictionary ---
     # 0. Get base parameter dictionary
     params = data.model_parameters_data
     # 1. Update params by susceptibility vector
     susceptibility = np.ones(no_ag)
-    # susc = susc_choices[-1]  # variable for possible FOR loop
     for susc in susc_choices:
         susceptibility[:4] = susc
         params.update({"susc": susceptibility})
         # 2. Update params by calculated BASELINE beta
-        # base_r0 = r0_choices[-1]  # variable for possible FOR loop
         for base_r0 in r0_choices:
             r0generator = R0Generator(param=params)
             beta = base_r0 / r0generator.get_eig_val(contact_mtx=contact_matrix,
                                                      susceptibles=susceptibles.reshape(1, -1),
                                                      population=population)[0]
             params.update({"beta": beta})
-
-            # Choose matrix type
-            # mtx_type = lower_matrix_types[0]  # variable for possible FOR loop
+            # 3. Choose matrix type
             for mtx_type in lower_matrix_types:
-
-                is_lhs_generated = True
                 if is_lhs_generated:
                     lower_limit_matrix = lower_limit_matrices[mtx_type]
 
@@ -97,14 +92,7 @@ def main():
                     os.makedirs("./sens_data/simulations", exist_ok=True)
                     filename = "./sens_data/simulations/simulation_" +\
                                "_".join([str(susc), str(base_r0), format(beta, '.5f'), str(mtx_type)])
-                    np.savetxt(fname=filename + ".csv",
-                               X=np.asarray(sim_output), delimiter=";")
-
-                    is_prcc_generated = False
-                    if is_prcc_generated:
-                        saved_simulation = np.loadtxt(filename + ".csv", delimiter=';')[:, 0:-no_ag]
-                        prcc_list = get_prcc_values(np.delete(saved_simulation, -2, axis=1))
-                        print(prcc_list)
+                    np.savetxt(fname=filename + ".csv", X=np.asarray(sim_output), delimiter=";")
                 else:
                     # Get modified contact matrix
                     cm_row = lower_limit_matrices[mtx_type][upper_tri_indexes]
@@ -114,18 +102,21 @@ def main():
                                                                 susceptibles=susceptibles.reshape(1, -1),
                                                                 population=population)[0]
                     print("R0 for modified contact matrix:", (beta / beta_cm) * base_r0)
-
                     # Get solution
-                    t = np.linspace(0, 500, 1000)
-                    solution = model.get_solution(t=t, parameters=params, cm=cm)
+                    t = np.linspace(0, 200, 500)
+                    plot_solution(t, params, cm, ["ih"])
 
-                    # Plot solution
-                    plt.plot(t, solution[:, ic_idx * no_ag:(ic_idx + 1) * no_ag])
-                    plt.show()
+
+def plot_solution(time, params, cm, compartments):
+    os.makedirs("./sens_data", exist_ok=True)
+    for comp in compartments:
+        solution = model.get_solution(t=time, parameters=params, cm=cm)
+        plt.plot(time, np.sum(solution[:, model.c_idx[comp] * no_ag:(model.c_idx[comp] + 1) * no_ag], axis=1))
+        plt.savefig('./sens_data/solution' + "_" + comp + '.pdf', format="pdf")
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     # plotter.generate_prcc_plots()
-    plotter.generate_stacked_plots()
-    plotter.plot_2d_contact_matrices()
+    # plotter.generate_stacked_plots()
+    # plotter.plot_2d_contact_matrices()
