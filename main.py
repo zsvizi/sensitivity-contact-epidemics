213d6c679734bb0ffd9a16d32a0934be6bdf8dbd
Norbert Bogya <bnorbert88@gmail.com>
2020-11-05 13:20:38 +0100

    Play with dynamics plots

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ import plotter
 data = DataLoader()
 
 # Define initial configs
-susc_choices = [0.5]  # [0.1, 0.5, 1.0]
-r0_choices = [2.2]  # [1.65, 2.2, 3]
-lower_matrix_types = ["home"]  # ["home", "normed"]
+susc_choices = [0.1, 0.5, 1.0]
+r0_choices = [1.65, 2.2, 3]
+lower_matrix_types = ["home", "normed"]
 no_ag = data.contact_data["home"].shape[0]
 model = RostModelHungary(model_data=data)
 population = model.population
@@ -41,7 +41,6 @@ def main():
 
     # Get contact matrices
 
-
     # --- Get complete parameters dictionary ---
     # 0. Get base parameter dictionary
     params = data.model_parameters_data
@@ -62,7 +61,7 @@ def main():
                 if is_lhs_generated:
                     lower_limit_matrix = lower_limit_matrices[mtx_type]
 
-                    number_of_samples = 10
+                    number_of_samples = 40000
                     lhs_table = create_latin_table(n_of_samples=number_of_samples,
                                                    lower=lower_limit_matrix[upper_tri_indexes],
                                                    upper=upper_limit_matrix[upper_tri_indexes])
@@ -90,29 +89,54 @@ def main():
 
                     os.makedirs("./sens_data", exist_ok=True)
                     os.makedirs("./sens_data/simulations", exist_ok=True)
-                    filename = "./sens_data/simulations/simulation_" +\
+                    filename = "./sens_data/simulations/simulation_Hungary_" + \
                                "_".join([str(susc), str(base_r0), format(beta, '.5f'), str(mtx_type)])
                     np.savetxt(fname=filename + ".csv", X=np.asarray(sim_output), delimiter=";")
                 else:
-                    # Get modified contact matrix
-                    cm_row = lower_limit_matrices[mtx_type][upper_tri_indexes]
-                    cm = get_contact_matrix_from_upper_triu(rvector=cm_row,
-                                                            age_vector=age_vector.reshape(-1, ))
-                    beta_cm = base_r0 / r0generator.get_eig_val(contact_mtx=cm,
-                                                                susceptibles=susceptibles.reshape(1, -1),
-                                                                population=population)[0]
-                    print("R0 for modified contact matrix:", (beta / beta_cm) * base_r0)
-                    # Get solution
-                    t = np.linspace(0, 200, 500)
-                    plot_solution(t, params, cm, ["ih"])
+                    if susc in [1.0] and base_r0 in [1.65] and mtx_type == "home":
+                        # Get modified contact matrix
+                        cm_row = lower_limit_matrices[mtx_type][upper_tri_indexes]
+
+                        # Total full contact matrix contact
+                        cm_full = upper_limit_matrix[upper_tri_indexes]
+
+                        # School closure
+                        contact_school = data.contact_data["school"]
+                        contact_school[3, :] = contact_school[:, 3] = 0
+                        full_contact_matrix = data.contact_data["home"] + data.contact_data["work"] + \
+                                              contact_school + data.contact_data["other"]
+                        cm_school_closure = (full_contact_matrix * age_vector)[upper_tri_indexes]
+
+                        cm1 = get_contact_matrix_from_upper_triu(rvector=cm_full,
+                                                                 age_vector=age_vector.reshape(-1, ))
+                        cm2 = get_contact_matrix_from_upper_triu(rvector=cm_school_closure,
+                                                                 age_vector=age_vector.reshape(-1, ))
+                        cm_list = [cm1, cm2]
+
+                        for cm in cm_list:
+                            beta_cm = base_r0 / r0generator.get_eig_val(contact_mtx=cm,
+                                                                        susceptibles=susceptibles.reshape(1, -1),
+                                                                        population=population)[0]
+                            print("R0 for modified contact matrix:", (beta / beta_cm) * base_r0)
+                        # Get solution
+                        t = np.linspace(0, 600, 1000)
+                        plot_solution(t, params, cm_list, ["ih", "ic"], "_".join([str(susc), str(base_r0)]))
 
 
-def plot_solution(time, params, cm, compartments):
-    os.makedirs("./sens_data", exist_ok=True)
-    for comp in compartments:
+def plot_solution(time, params, cm_list, compartments, title_part):
+    os.makedirs("./sens_data/dinamics", exist_ok=True)
+    # for comp in compartments:
+    for cm in cm_list:
         solution = model.get_solution(t=time, parameters=params, cm=cm)
-        plt.plot(time, np.sum(solution[:, model.c_idx[comp] * no_ag:(model.c_idx[comp] + 1) * no_ag], axis=1))
-        plt.savefig('./sens_data/solution' + "_" + comp + '.pdf', format="pdf")
+        plt.plot(time, np.sum(solution[:, model.c_idx["ih"] * no_ag:(model.c_idx["ih"] + 1) * no_ag], axis=1) +
+                       np.sum(solution[:, model.c_idx["icr"] * no_ag:(model.c_idx["icr"] + 1) * no_ag], axis=1))
+        plt.savefig('./sens_data/dinamics/solution' + "_" + title_part + "_" + "ihicr" + '.pdf', format="pdf")
+    plt.close()
+    # for cm in cm_list:
+    #     solution = model.get_solution(t=time, parameters=params, cm=cm)
+    #     plt.plot(time, np.sum(solution[:, model.c_idx["c"] * no_ag:(model.c_idx["c"] + 1) * no_ag], axis=1))
+    #     plt.savefig('./sens_data/dinamics/solution' + "_" + title_part + "_" + "c" + '.pdf', format="pdf")
+    # plt.close()
 
 
 if __name__ == "__main__":
