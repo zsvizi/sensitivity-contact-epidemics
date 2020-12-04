68b4df2e19a48031a5ac72109f89b72c48e1f5a5
Zsolt Vizi <vizizsolt89@gmail.com>
2020-12-04 10:23:24 +0100

    Add transform in LHS input

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from analysis import Analysis
 from dataloader import DataLoader
 from model import RostModelHungary
 from plotter import generate_prcc_plots
-from prcc import create_latin_table, get_contact_matrix_from_upper_triu
+from prcc import create_latin_table
 from r0 import R0Generator
 
 
@@ -25,10 +25,11 @@ class Simulation:
         self._get_initial_config()
 
         # User-defined lower matrix types
-        self.lower_limit_matrices = \
-            {"normed": self.upper_limit_matrix - np.min(self.upper_limit_matrix - self.contact_home * self.age_vector),
-             "home": self.contact_home * self.age_vector}
-        self.lower_matrix_types = list(self.lower_limit_matrices.keys())
+        self.lhs_boundaries = \
+            {"unit": {"lower": np.zeros(self.no_ag),
+                      "upper": np.ones(self.no_ag) * self._get_upper_bound_factor_unit()}
+             }
+        self.lower_matrix_types = list(self.lhs_boundaries.keys())
 
     def run(self):
         is_lhs_generated = False
@@ -59,45 +60,54 @@ class Simulation:
 
     def generate_lhs(self, base_r0, beta, mtx_type, susc, r0generator):
         # Get actual lower limit matrix
-        lower_limit_matrix = self.lower_limit_matrices[mtx_type]
+        lower_bound = self.lhs_boundaries[mtx_type]["lower"]
+        upper_bound = self.lhs_boundaries[mtx_type]["upper"]
 
         # Get LHS tables
         number_of_samples = 40000
         lhs_table = create_latin_table(n_of_samples=number_of_samples,
-                                       lower=lower_limit_matrix[self.upper_tri_indexes],
-                                       upper=self.upper_limit_matrix[self.upper_tri_indexes])
+                                       lower=lower_bound,
+                                       upper=upper_bound)
         print("Simulation for", number_of_samples,
               "samples (", "-".join([str(susc), str(base_r0), mtx_type]), ")")
         sleep(0.3)
 
         # Local function for calculating LHS output
-        def get_lhs_output(cm_rvector):
-            cm_ndarray = get_contact_matrix_from_upper_triu(rvector=cm_rvector,
-                                                            age_vector=self.age_vector.reshape(-1, ))
-            beta_lhs = base_r0 / r0generator.get_eig_val(contact_mtx=cm_ndarray,
+        def get_simulation_output(lhs_sample):
+            cm_total_sim = self.contact_matrix * self.age_vector - lhs_sample.reshape(-1, 1)
+            cm_sim = cm_total_sim / self.age_vector
+            beta_lhs = base_r0 / r0generator.get_eig_val(contact_mtx=cm_sim,
                                                          susceptibles=self.susceptibles.reshape(1, -1),
                                                          population=self.population)[0]
             r0_lhs = (beta / beta_lhs) * base_r0
-            output = np.append(cm_rvector, [0, r0_lhs])
+            output = np.append(cm_total_sim[self.upper_tri_indexes], [0, r0_lhs])
             output = np.append(output, np.zeros(self.no_ag))
             return list(output)
 
         # Generate LHS output
-        results = list(tqdm(map(get_lhs_output, lhs_table), total=lhs_table.shape[0]))
+        results = list(tqdm(map(get_simulation_output, lhs_table), total=lhs_table.shape[0]))
         results = np.array(results)
 
-        # Append R0 values
+        # Sort tables by R0 values
         r0_col_idx = int((self.no_ag + 1) * self.no_ag / 2 + 1)
-        results = results[results[:, r0_col_idx].argsort()]
+        sorted_idx = results[:, r0_col_idx].argsort()
+        results = results[sorted_idx]
+        lhs_table = np.array(lhs_table[sorted_idx])
         sim_output = np.array(results)
         sleep(0.3)
 
-        # Save LHS output
+        # Create directories for saving calculation outputs
         os.makedirs("./sens_data", exist_ok=True)
         os.makedirs("./sens_data/simulations", exist_ok=True)
+        os.makedirs("./sens_data/lhs", exist_ok=True)
+        # Save simulation input
         filename = "./sens_data/simulations/simulation_Hungary_" + \
                    "_".join([str(susc), str(base_r0), format(beta, '.5f'), str(mtx_type)])
         np.savetxt(fname=filename + ".csv", X=np.asarray(sim_output), delimiter=";")
+        # Save LHS output
+        filename = "./sens_data/lhs/lhs_Hungary_" + \
+                   "_".join([str(susc), str(base_r0), format(beta, '.5f'), str(mtx_type)])
+        np.savetxt(fname=filename + ".csv", X=np.asarray(lhs_table), delimiter=";")
 
     def _get_initial_config(self):
         self.no_ag = self.data.contact_data["home"].shape[0]
@@ -109,10 +119,14 @@ class Simulation:
         self.contact_matrix = self.data.contact_data["home"] + self.data.contact_data["work"] + \
             self.data.contact_data["school"] + self.data.contact_data["other"]
         self.contact_home = self.data.contact_data["home"]
-        self.upper_limit_matrix = self.contact_matrix * self.age_vector
         self.upper_tri_indexes = np.triu_indices(self.no_ag)
         # 0. Get base parameter dictionary
         self.params = self.data.model_parameters_data
+
+    def _get_upper_bound_factor_unit(self):
+        cm_diff = (self.contact_matrix - self.contact_home) * self.age_vector
+        min_diff = self.no_ag * np.min(cm_diff) / 2
+        return min_diff
 
 
 def main():
