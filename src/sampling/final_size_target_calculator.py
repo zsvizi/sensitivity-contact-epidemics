450e25ee6cc7dc52d82d7a16475bce9989ec8cb9
Zsolt Vizi <vizizsolt89@gmail.com>
2023-09-21 18:16:53 +0200

    Finalize ODE solving logic

--- src/sampling/final_size_target_calculator.py
+++ src/sampling/final_size_target_calculator.py
@@ -6,8 +6,6 @@ from src.sampling.target_calculator import TargetCalculator
 class FinalSizeTargetCalculator(TargetCalculator):
     def __init__(self, sim_obj: SimulationNPI):
         super().__init__(sim_obj=sim_obj)
-        self.hospitalized = np.array([])
-        self.total_infectious = np.array([])
 
     def get_output(self, cm: np.ndarray):
         t_interval = 10
@@ -20,65 +18,70 @@ class FinalSizeTargetCalculator(TargetCalculator):
             cm=cm)
 
         state = sol[-1]
-        sol2 = []
 
         # introduce a variable for tracking how long the ODE was solved
         t_interval_complete = 0
         t_interval_complete += t_interval
 
-        hospital_peak = (self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["ih"]) +
-                                 self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["ic"]) +
-                                 self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["icr"])).max()
+        # let's say we want to store the peak size of hospitalized people during the simulation
+        # aggregate age groups for ih, ic and icr from the previously calculated solution
+        # then get maximal value of the time series
+        hospital_peak = (
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["ih"]) +
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["ic"]) +
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["icr"])
+        ).max()
 
         # Loop infinitely
         while True:
-            hospital_peak_now = (self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["ih"])[-1] +
-                                 self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["ic"])[-1] +
-                                 self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["icr"])[-1]).max()
+            # let's say we want to store the peak size of hospitalized people during the simulation
+            # aggregate age groups for ih, ic and icr from the previously calculated solution
+            # then get maximal value of the time series
+            hospital_peak_now = (
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["ih"])[-1] +
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["ic"])[-1] +
+                self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx["icr"])[-1]
+            ).max()
 
             # check whether it is higher than it was in the previous turn
             if hospital_peak_now > hospital_peak:
                 hospital_peak = hospital_peak_now
-                self.hospitalized = hospital_peak
 
-            n_infecteds = self.sim_obj.model.aggregate_by_age(
-                solution=sol, idx=self.sim_obj.model.c_idx["c"])[-1] + \
-                          (self.sim_obj.model.aggregate_by_age(
-                              solution=sol, idx=self.sim_obj.model.c_idx["l1"])[-1] +
-                           self.sim_obj.model.aggregate_by_age(
-                               solution=sol, idx=self.sim_obj.model.c_idx["l2"])[-1] +
-                           self.sim_obj.model.aggregate_by_age(
-                               solution=sol, idx=self.sim_obj.model.c_idx["ih"])[-1] +
-                           self.sim_obj.model.aggregate_by_age(
-                               solution=sol, idx=self.sim_obj.model.c_idx["ic"])[-1] +
-                           self.sim_obj.model.aggregate_by_age(
-                               solution=sol, idx=self.sim_obj.model.c_idx["icr"])[-1] -
-                           self.sim_obj.model.aggregate_by_age(
-                               solution=sol, idx=self.sim_obj.model.c_idx["d"])[-1]
-                           )
-            self.total_infectious = n_infecteds
+            # calculate the number of infected individuals at the current state
+            # sum of aggregation along all age groups in
+            # l1, l2, ip, ia1, ia2, ia3, is1, is2, is3, ih, ic, icr
+            n_infecteds = 0
+            inf_comps = ["l1", "l2", "ip", "ia1", "ia2", "ia3", "is1", "is2", "is3", "ih", "ic", "icr"]
+            for cmp in inf_comps:
+                n_infecteds += self.sim_obj.model.aggregate_by_age(
+                    solution=sol, idx=self.sim_obj.model.c_idx[cmp])[-1]
 
             # check whether the previously calculated number is less than 1
             if n_infecteds < 1:
                 break
+
             # since the number of infecteds is above 1, we solve the ODE again
             # from 0 to t_interval using the current state
-            sol2 = self.sim_obj.model.get_solution(
+            sol = self.sim_obj.model.get_solution(
                 init_values=state,
                 t=t,
                 parameters=self.sim_obj.params,
                 cm=cm)
-            state = sol2[-1]
+            state = sol[-1]
             t_interval_complete += t_interval
-        sol2 = sol2
+
+        # for calculating final size value for compartment D
+        # aggregate the current state (calculated in the last turn of the loop) for compartment "d"
         final_size_dead = np.sum(self.sim_obj.model.aggregate_by_age(
-            solution=sol2,
+            solution=sol,
             idx=self.sim_obj.model.c_idx["d"])[-1])
+
+        # concatenate all output values to get the array of return
         output = np.array([final_size_dead])
         return output
