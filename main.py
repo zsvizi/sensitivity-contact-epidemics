66d8f874845c082ed6c9af59937878bf63a3a045
zoltanocska <zolysta@gmail.com>
2025-11-12 02:08:31 +0100

    Changed main.py according to Revision branch

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from src.dataloader import DataLoader
 
 
 def main():
-    data = DataLoader(country="united_states")
+    data = DataLoader(country="Hungary_maszk")
     simulation = src.SimulationNPI(data=data,
                                    n_samples=10000,
-                                   epi_model="moghadas",
-                                   country="united_states")
+                                   epi_model="rost_maszk",
+                                   country="Hungary_maszk",
+                                   strategy="baseline",
+                                   is_kappa_applied=False)
     simulation.generate_lhs()
     simulation.calculate_prcc_values()
     simulation.plot_prcc_values()
-    simulation.generate_analysis_results()
-    simulation.plot_max_values_contact_manipulation()
+    # simulation.generate_analysis_results()
+    # simulation.plot_max_values_contact_manipulation()
 
 
 if __name__ == '__main__':
     main()
-
-
